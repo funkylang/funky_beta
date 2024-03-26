85a16793534dffb16606fed623e63c932cd51894
Michael Niederle <mniederle@gmx.at>
2024-03-26 16:44:18 +0100

    Added missing files

---
+++ basic/types/meta.c
@@ -0,0 +1,74 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#include "runtime/common.h"
+#include "runtime/builtins.h"
+#include "runtime/fko.h"
+#include "runtime/memory.h"
+
+
+enum {
+  var__START = FIRST_VAR-1,
+  var_std__neutral_element_of, // attribute
+  var_std__assign, // extern
+  var_std_types__object, // extern
+  var_neutral_element_of, // extern polymorphic
+  var_undefined, // extern
+  var__END
+};
+
+
+static ATTRIBUTE_DEFINITION std_types__object__attributes[] = {
+  {-var_neutral_element_of, -var_undefined}
+};
+
+static FUNKY_VARIABLE variables_table[] = {
+  {
+    FOT_POLYMORPHIC, 0, 0,
+    "neutral_element_of\000std", NULL,
+    {.has_a_setter = false}
+  },
+  {
+    FOT_UNKNOWN, 0, 0,
+    "assign\000std", NULL,
+    {.position = POS(23, 1)}
+  },
+  {
+    FOT_UNKNOWN, 0, 1,
+    "object\000std_types", std_types__object__attributes,
+    {.position = POS(27, 1)}
+  },
+  {
+    FOT_UNKNOWN_POLYMORPHIC, 0, 0,
+    "neutral_element_of\000", NULL,
+    {.position = POS(27, 20)}
+  },
+  {
+    FOT_UNKNOWN, 0, 0,
+    "undefined\000", NULL,
+    {.position = POS(27, 39)}
+  }
+};
+
+static FUNKY_NAMESPACE used_namespaces[] = {
+  {"std", 1, 0}
+};
+
+FUNKY_MODULE module__basic__types__meta = {
+  "_basic__types__meta", // module name
+  "basic/types/meta.fky", // module filename
+  .major_version = 0,
+  .minor_version = 0,
+  .feature_flags = FEAT_POSITIONS,
+  .marker = 0,
+  0, // number of required modules
+  0, // number of defined namespaces
+  1, // number of used namespaces
+  0, // number of constants
+  5, // number of variables
+  NULL, // required modules
+  NULL, // defined namespaces
+  used_namespaces,
+  NULL, // no constants
+  variables_table,
+};
